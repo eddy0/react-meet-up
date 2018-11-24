fe8488b61341ef7c36060fca6c25ab80d6398562
eddy0 <awindy369@gmail.com>
2018-11-24 00:25:41 -0800

    issue on create form

--- src/component/event/EventDashboard/EventDashboard.tsx
+++ src/component/event/EventDashboard/EventDashboard.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react'
 import Grid from '@material-ui/core/Grid'
 import EventList from '../EventList/EventList'
-import { events } from '../../../utils/DATA'
-import EventForm from './../EventForm/EventForm'
+import { events, IEvent } from '../../../utils/DATA'
+import EventForm, { FormValue } from './../EventForm/EventForm'
 import Button from '@material-ui/core/Button'
 
 interface IEventDashboardState {
-  isOpen: boolean
+  isOpen: boolean,
+  events: IEvent[]
 }
+
 
 class EventDashboard extends React.Component<{}, IEventDashboardState> {
   state: IEventDashboardState = {
     isOpen: false,
+    events: []
+  }
+
+  componentDidMount() {
+    this.setState((prevState: IEventDashboardState) => {
+      return {
+        ...prevState,
+        events: events
+      }
+    })
   }
 
   handleToggleForm = (e:React.MouseEvent<HTMLButtonElement>):void => {
@@ -23,20 +35,30 @@ class EventDashboard extends React.Component<{}, IEventDashboardState> {
     })
   }
 
+  createEvent = (form:FormValue) => {
+    console.log(form)
+    this.setState((prevState) => {
+      return  {
+        ...prevState,
+        events: prevState.events.concat(form)
+      }
+    })
+  }
+
   render() {
     return (
       <div className='row'>
         <Grid container item xs={12}>
           <Grid container item xs={8} justify='center'>
             <h3>这是 hot load</h3>
-            <EventList events={events} />
+            <EventList events={this.state.events} />
           </Grid>
           <Grid container item xs={4} direction='column' alignItems='center'>
             <h4>right is hot fast</h4>
         <Button color='primary' variant='contained' onClick={this.handleToggleForm}>
               create form
             </Button>
-            {this.state.isOpen && <EventForm />}
+            {this.state.isOpen && <EventForm createEvent={this.createEvent} />}
           </Grid>
         </Grid>
       </div>
--- src/component/event/EventForm/EventForm.tsx
+++ src/component/event/EventForm/EventForm.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { TextField } from '@material-ui/core'
 import { useState } from 'react'
+import Button from '@material-ui/core/Button';
 
-interface FormValue {
+export interface FormValue {
   name: string
   username: string
   password: string
@@ -17,8 +18,12 @@ const state: FormValue = {
 }
 
 
+interface Iprops {
+  createEvent(form:FormValue):void
+}
 
-const EventForm: React.SFC = (props) => {
+
+const EventForm: React.SFC<Iprops> = (props:Iprops) => {
   const [form, update] = useState(state)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -30,12 +35,18 @@ const EventForm: React.SFC = (props) => {
     })
   }
 
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    props.createEvent(form)
+  }
+
   return (
-    <form autoComplete='off'>
+    <form autoComplete='off' onSubmit={handleSubmit}>
       <TextField id='name' name='name' label='name' value={form.name} onChange={handleChange} />
       <TextField id='username' name='username' label='username' value={form.username} onChange={handleChange} />
       <TextField id='password' name='password' label='password' value={form.password} onChange={handleChange} />
       <TextField id='todo' name='todo' label='todo' value={form.todo} onChange={handleChange} />
+      <Button type='submit' color="secondary" variant='contained' >create a new event</Button>
     </form>
   )
 }
