e1315b4cf257de9b911f643b9ac11f352a2ddb69
eddy0 <awindy369@gmail.com>
2018-11-28 20:47:54 -0800

    分离枚举

--- src/action/eventAction.tsx
+++ src/action/eventAction.tsx
@@ -5,7 +5,7 @@ function handleActionFetchEvent(events: IEvent[]) {
     dispatch(actionFetchEvent(events))
   }
 }
-
+// 这个函数返回的是 Action 类型, 其中的 payload 是 Ievent 类型的数组
 const actionFetchEvent = (events:IEvent[]): Action<IEvent[]> => {
   return {
     type: ActionType.FETCH_EVENT,
--- src/model/model.tsx
+++ src/model/model.tsx
@@ -23,30 +23,36 @@ export interface Todo {
   complete: boolean
 }
 
-// type EventType = {
-//   CREATE_EVENT:string = 'CREATE_EVENT',
-//   UPDATE_EVENT:string = 'UPDATE_EVENT',
-//   DELETE_EVENT:string = 'DELETE_EVENT',
-// }
-
-// type TodoType = {
-//   ADD_TODO:string = 'ADD_TODO',
-//   DELETE_TODO:string = 'DELETE_TODO',
-//   COMPLETE_TODO:string = 'COMPLETE_TODO',
-//   TOGGLE_TODO:string = 'TOGGLE_TODO',
-// }
-
-// 如何分离不同的 enum
-export enum ActionType {
+enum EventType {
   FETCH_EVENT = 'FETCH_EVENT',
   CREATE_EVENT = 'CREATE_EVENT',
   UPDATE_EVENT = 'UPDATE_EVENT',
   DELETE_EVENT = 'DELETE_EVENT',
+}
+
+enum TodoType {
   ADD_TODO = 'ADD_TODO',
   DELETE_TODO = 'DELETE_TODO',
   COMPLETE_TODO = 'COMPLETE_TODO',
   TOGGLE_TODO = 'TOGGLE_TODO',
 }
+
+export const ActionType =  {
+  ...EventType,
+  ...TodoType
+}
+
+// 如何分离不同的 enum
+// export enum ActionType {
+//   FETCH_EVENT = 'FETCH_EVENT',
+//   CREATE_EVENT = 'CREATE_EVENT',
+//   UPDATE_EVENT = 'UPDATE_EVENT',
+//   DELETE_EVENT = 'DELETE_EVENT',
+//   ADD_TODO = 'ADD_TODO',
+//   DELETE_TODO = 'DELETE_TODO',
+//   COMPLETE_TODO = 'COMPLETE_TODO',
+//   TOGGLE_TODO = 'TOGGLE_TODO',
+// }
 
 
 // export interface Action<T> {
@@ -56,6 +62,7 @@ export enum ActionType {
 //   [payload: string]: T | ActionType
 // }
 
+// action 传入的类型 T, 与 payload 的类型一致 
 export interface Action<T> {
   type: string, 
   // 如何做到分开不同的类型???
