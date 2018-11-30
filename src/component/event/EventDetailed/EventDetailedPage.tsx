c8c457b42e7f53d773904e862fcdbc70691833ff
eddy0 <awindy369@gmail.com>
2018-11-29 22:25:14 -0800

    !!!注意router的component如果要加入id需要这么处理interface  ...interface EventDetailedPageProps extends RouteComponentProps<{id: string}>{}

--- src/component/event/EventDetailed/EventDetailedPage.tsx
+++ src/component/event/EventDetailed/EventDetailedPage.tsx
@@ -4,8 +4,15 @@ import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
+import { connect } from 'react-redux';
+import { StoreState } from './../../../reducer/index';
+import { IEvent } from 'src/model/model';
+import { RouteComponentProps } from 'react-router-dom';
 
-interface EventDetailedPageProps {}
+interface EventDetailedPageProps extends RouteComponentProps<{id: string}>{
+
+}
+
 
 const EventDetailedPage: React.SFC<EventDetailedPageProps> = (props) => {
   return (
@@ -25,4 +32,16 @@ const EventDetailedPage: React.SFC<EventDetailedPageProps> = (props) => {
   )
 }
 
-export default EventDetailedPage
+
+type OwnProps = RouteComponentProps<{id: string}>;
+
+const mapStateToProps = (state:StoreState, props: OwnProps) => {
+  const id = props.match.params.id
+  let event:IEvent = state.events.filter((event:IEvent) => event.id === id)[0]
+  return {
+    event: event
+  }
+}
+
+export default connect(mapStateToProps)(EventDetailedPage)
+
--- src/component/event/EventDetailed/EventDetailedSidebar.tsx
+++ src/component/event/EventDetailed/EventDetailedSidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Paper, Typography, Avatar, Chip } from '@material-ui/core'
+import { Paper, Typography, Avatar, Chip, Badge, Divider, Tooltip } from '@material-ui/core'
 import styled from 'styled-components'
 import { PaperProps } from '@material-ui/core/Paper'
 
@@ -24,16 +24,20 @@ const EventDetailedSidebar: React.SFC<EventDetailedSidebarProps> = (props) => {
       <Typography gutterBottom={true} align={'center'} variant='h5'>
         2 people is going
       </Typography>
+      <Divider />
       <ChipArea>
+        <Tooltip title='host'>
+          <Chip label='Basic Chip' color='primary' avatar={<Avatar src='/assets/images/user.png'>MB</Avatar>} />
+        </Tooltip>
         <Chip label='Basic Chip' variant='outlined' avatar={<Avatar src='/assets/images/user.png'>MB</Avatar>} />
-        <Chip label='Basic Chip' variant='outlined' avatar={<Avatar src='/assets/images/user.png'>MB</Avatar>} />
         <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
         <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
         <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
         <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
         <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
-        <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>} />
-
+        <Badge badgeContent='host' color='primary' >
+          <Chip label='Basic Chip' avatar={<Avatar>MB</Avatar>}/>
+        </Badge>
       </ChipArea>
     </SideBar>
   )
