ae6b40f961422692628e1b1c78246f5dad300751
eddy <awindy369@gmail.com>
2018-12-14 23:10:30 -0800

    add signed in menu

---
+++ src/app/component/nav/Menus/SignedInMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SignedInMenu extends Component {
+  render() {
+    return (
+      <div>
+        signed in
+
+      </div>
+    )
+  }
+}
+
+export default SignedInMenu
