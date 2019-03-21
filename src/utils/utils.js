b08aa8623a48505086e1a23c566865ae18dce72e
eddy <awindy369@gmail.com>
2019-03-20 23:52:58 -0700

    add create data tree

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -66,9 +66,21 @@ const formatChats = (chats) => {
 }
 
 
+const createDataTree = dataset => {
+    let hashTable = Object.create(null);
+    dataset.forEach(a => hashTable[a.id] = {...a, childNodes: []});
+    let dataTree = [];
+    dataset.forEach(a => {
+        if (a.parentId) hashTable[a.parentId].childNodes.push(hashTable[a.id]);
+        else dataTree.push(hashTable[a.id])
+    });
+    return dataTree
+};
+
 export {
   log,
   errorMessage,
   resetForm,
+  createDataTree,
   objectToArray,
 }
